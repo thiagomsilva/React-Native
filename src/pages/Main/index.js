383c86608853b955a15224584c10859dedbf983a
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2019-10-27 00:45:13 -0300

    commit pages user repository

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Keyboard, ActivityIndicator} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -19,6 +20,16 @@ import {
 } from './styles';
 
 export default class Main extends Component {
+    static navigationOptions = {
+        title: 'Usuários',
+    };
+
+    static propTypes = {
+        navigation: PropTypes.shape({
+            navigate: PropTypes.func,
+        }).isRequired,
+    };
+
     state = {
         newUser: '',
         users: [],
@@ -64,6 +75,12 @@ export default class Main extends Component {
         Keyboard.dismiss(); // esconde o teclado após consulta
     };
 
+    handleNavigate = user => {
+        const {navigation} = this.props;
+
+        navigation.navigate('User', {user});
+    };
+
     render() {
         const {users, newUser, loading} = this.state;
 
@@ -99,7 +116,8 @@ export default class Main extends Component {
                             <Name>{item.name}</Name>
                             <Bio>{item.bio}</Bio>
 
-                            <ProfileButton onPress={() => {}}>
+                            <ProfileButton
+                                onPress={() => this.handleNavigate(item)}>
                                 <ProfileButtonText>
                                     Ver Perfil
                                 </ProfileButtonText>
@@ -111,7 +129,3 @@ export default class Main extends Component {
         );
     }
 }
-
-Main.navigationOptions = {
-    title: 'Usuários',
-};
